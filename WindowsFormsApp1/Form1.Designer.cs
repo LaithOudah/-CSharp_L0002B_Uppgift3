04ac91712eef9128ab9016612565ef35bc261f6f
Laith Oudah <laithoudah@gmail.com>
2021-11-25 22:40:11 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Identity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Välkommen!
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Kristen ITC", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Crimson;
+            this.label4.Location = new System.Drawing.Point(10, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(205, 44);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Välkommen!";
+            // 
+            // Uppmaning att skriva sitt personnummer i textrutorna nedan.
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(2, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(217, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Skriv in ditt namn och personnummer nedan.";
+            // 
+            // Uppmanar personen att skriva sitt förnamn!
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Förnamn:";
+            // 
+            // Skriv ditt efternamn.
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Efternamn:";
+            // 
+            // Skriv ditt personnummer i rätt format.
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Personummer:\n(ååååmmdd - xxxx)";
+            // 
+            // Här skriver man sitt förnamn.
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Efternamn...
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Personnummer...
+            // 
+            this.textBox3.Location = new System.Drawing.Point(95, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // Klicka på kör och programmet körs.
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Kör!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Här dyker ett hemligt ord upp vid klickning på Kör!
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 23);
+            this.label6.TabIndex = 10;
+            // 
+            // Här skrivs resultatet ut.
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.richTextBox1.Location = new System.Drawing.Point(1, 210);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(212, 159);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // Avsluta programmet
+            // 
+            this.button2.Location = new System.Drawing.Point(1, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Avsluta";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Identity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 419);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Identity";
+            this.Text = "ID-Check";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Identity : Form
+    {
+        public Identity()
+        {
+            InitializeComponent();
+        }
+        public void button1_Click(object sender, EventArgs e) //Kör själva beräkningen vid klickning.
+        {
+            label6.Text = "Resultat:"; //Skriver ut "Resultat" i den dolda rutan.
+            string fname = textBox1.Text;
+            string lname = textBox2.Text;
+            string pNumber = textBox3.Text;
+            if (pNumber.Length == 13) //Kontrollerar så att formatet är korrekt, annars får man felmeddelande.
+            { //Här startar beräkningarna så att man i slutändan får ut ett giltigt personnummer.
+                int a0 = pNumber[2] - 48; //Eftersom pNumber[] är en string så får jag ta talet - 0 för att få rätt sluttal. Ex; "2" - "0" = 50- 48 = 2.
+                int a1 = pNumber[3] - 48;
+                int a2 = pNumber[4] - 48;
+                int a3 = pNumber[5] - 48;
+                int a4 = pNumber[6] - 48;
+                int a5 = pNumber[7] - 48;
+                int a6 = pNumber[9] - 48;
+                int a7 = pNumber[10] - 48;
+                int a8 = pNumber[11] - 48;
+                int a9 = pNumber[12] - 48;
+                //Eftersom det endast är värdet på a0, a6 och a8 som kan överstiga 10 så räcker det att köra nedanstående för dem.
+                if (a0 * 2 >= 10)
+                {
+                    string a01 = (a0 * 2).ToString(); //Konverterar det multiplicerade talet till en string.
+                    a0 = a01[0] + a01[1] - 96; //Lägger ihop char 1 och 2 till en summa - 2*"0".
+                }
+                else
+                {
+                    a0 = a0 * 2; //Multiplicera med 2.
+                }
+                if (a6 * 2 >= 10)
+                {
+                    string a61 = (a6 * 2).ToString();
+                    a6 = a61[0] + a61[1] - 96;
+                }
+                else
+                {
+                    a6 = a6 * 2;
+                }
+                if (a8 * 2 >= 10)
+                {
+                    string a81 = (a8 * 2).ToString();
+                    a8 = a81[0] + a81[1] - 96;
+                }
+                else
+                {
+                    a8 = a8 * 2;
+                }
+                double value0 = a0 + a1 + a3 + a5 + a6 + a7 + a8 + a9 + ((a2 + a4) * 2); //Beräknar summan av samtliga deltal.
+                int p = pNumber[11] - 48; //Könskontroll!
+                char y1 = pNumber[0]; //Millenium.
+                char y2 = pNumber[1]; //Sekel.
+                char y3 = pNumber[2]; //Decennium.
+                char y4 = pNumber[3]; //År.
+                double d = value0 / 10; //Bara en förenkling av value0.
+
+                if ((d == 1) || (d == 2) || (d == 3) || (d == 4) || (d == 5) || (d == 6) || (d == 7)) //Om d är ett heltal, kör på! Annars felmeddelande.
+                {
+
+                    if ((p == 1) || (p == 3) || (p == 5) || (p == 7) || (p == 9)) //Könskontroll för udda siffror.
+                    { //Skriver ut texten i den nedersta rutan.
+                        richTextBox1.Text = ("Förnamn: " + fname + "\n" + "Efternamn: " + lname + "\n" + "Juridiskt kön: Man" + "\n" + "Födelseår: " + y1 + y2 + y3 + y4);
+                    }
+                    else //Jämn siffra = Kvinna
+                    { //Skriver ut texten i den nedersta rutan.
+                        richTextBox1.Text = ("Förnamn: " + fname + "\n" + "Efternamn: " + lname + "\n" + "Juridiskt kön: Kvinna" + "\n" + "Födelseår: " + y1 + y2 + y3 + y4);
+                    }
+                }
+                else //Felmeddelande
+                {
+                    richTextBox1.Text = ("Felaktigt personnummer!" + "\n" + "Försök igen!");
+                }
+
+            }
+            else //Felmeddelande
+            {
+                richTextBox1.Text = ("Felaktigt personnummer!" + "\n" + "Försök igen!");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e) //Stänger programmet.
+        {
+            this.Close();
+        }
+    }
+}
